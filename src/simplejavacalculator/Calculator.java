113888e8a264dd25bb96fd4529e3dcecaa5107e4
mokbor <43931480+mokbor@users.noreply.github.com>
2019-01-12 23:33:54 +0100

    Update Calculator.java

--- src/simplejavacalculator/Calculator.java
+++ src/simplejavacalculator/Calculator.java
@@ -35,6 +35,9 @@ public class Calculator {
         if (mode == BiOperatorModes.xpowerofy) {
             return pow(num1,num2);
         }
+        if ( 1==2) {
+        	System.out.println("dupa");
+        }
 
         // never reach
         throw new Error();
