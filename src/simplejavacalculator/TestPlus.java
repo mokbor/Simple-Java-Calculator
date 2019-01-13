9824d7437812ea592d250579466d5c0423d71f53
mokbor <43931480+mokbor@users.noreply.github.com>
2019-01-13 04:10:45 +0100

    4

--- src/simplejavacalculator/TestPlus.java
+++ src/simplejavacalculator/TestPlus.java
@@ -1,22 +1,23 @@
 package simplejavacalculator;
 
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
 
 import simplejavacalculator.Calculator.BiOperatorModes;
 
-class Test {
+public class TestPlus {
 
 	@Test
-	void testPlus() {
+	public void test() {
 		Calculator.BiOperatorModes mode = BiOperatorModes.add;
 		Calculator calc = new Calculator();
 		calc.setNum1(21.0);
 		calc.setMode(mode);
 		calc.setNum2(8.99);
 		Double wynik = calc.calculateBiImpl();
-		assertEquals(29.99, wynik, 0);
-		
-		
+		assertEquals(29.99, wynik, 0.0000001);
 	}
 
 }
